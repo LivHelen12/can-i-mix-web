6c7781bc563b9a54c41fc8d18c7b0f2b7eab9367
Livia Helen <liviahelen025@gmail.com>
2023-02-11 20:04:51 -0300

    Add empty space

--- src/components/specialist/footer/index.tsx
+++ src/components/specialist/footer/index.tsx
@@ -9,7 +9,7 @@ export const Footer = ({ post }: Props) => {
     <div className="fixed bottom-0 left-0 z-20 w-full p-4 shadow md:flex md:items-center md:justify-around md:p-6 dark:bg-slate-100 dark:border-gray-400 text-slate-400">
       <p>Publicado pelo(a) {post?.specialist?.name}</p>
       <p>
-        {post?.specialist?.education?.name} em
+        {post?.specialist?.education?.name} em {""}
         {post?.specialist?.fieldOfWork?.name}
       </p>
     </div>
